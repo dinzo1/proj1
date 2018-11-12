1215eb59144d6c7e9acc50299247d25a1b10e1cc
Dinzo <dinzo@mail.ru>
2018-11-12 16:32:11 +0300

    New version in new branch

--- WindowsFormsApp26/Form1.Designer.cs
+++ WindowsFormsApp26/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(152, 61);
             this.button1.TabIndex = 0;
-            this.button1.Text = "START!";
+            this.button1.Text = "START! New Edit!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- WindowsFormsApp26/Form1.cs
+++ WindowsFormsApp26/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp26
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("All OK!!!");
+            MessageBox.Show("All OK!!! New edit!");
         }
     }
 }
