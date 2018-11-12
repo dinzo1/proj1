7d68293ca1d60a0339daf18779551444f3181e3c
Dinzo <dinzo@mail.ru>
2018-11-12 16:27:41 +0300

    START

--- WindowsFormsApp26/Form1.Designer.cs
+++ WindowsFormsApp26/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(152, 61);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Gooooo!";
+            this.button1.Text = "START!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -48,7 +48,7 @@
             this.ClientSize = new System.Drawing.Size(489, 230);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "GIT Test V1";
+            this.Text = "GIT Test";
             this.ResumeLayout(false);
 
         }
